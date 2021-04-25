2d700636ce871bbdfeec62070f941bce4f43a65d
Alex Shaver <alexshaver@gmail.com>
2021-04-25 14:24:54 -0400

    Start a performance test with the expiration deadlines in them
    During this test, of the roughly 29 seconds that elapse, 19 of them are during the set up phase, which makes assigning deadlines a bit difficult. However, in the current settings, I encountered ~20% expired entries which is a pretty reasonable statistic.

--- src/tests/KeyValueStorePerformanceTest.cpp
+++ src/tests/KeyValueStorePerformanceTest.cpp
@@ -30,7 +30,7 @@ constexpr bool DEBUG_TEST{false};
 //constexpr int microsecondResolution{1};
 constexpr int microsecondResolution{100};
 
-constexpr size_t pairCount{DEBUG_TEST ? 100 : 10'000'000};
+constexpr size_t pairCount{DEBUG_TEST ? 10000 : 10'000'000};
 
 // choosing random, but fixed, seed for reproducibility
 std::mt19937_64 rng{0xede26709};
@@ -38,6 +38,8 @@ std::mt19937_64 rng{0xede26709};
 // for the data, to keep it simple to generate, we'll use 'double' as our data type
 std::uniform_int_distribution<size_t> keyDistribution{0, SIZE_MAX - 1};
 std::uniform_real_distribution<double> valueDistribution{-1000, 1000};
+
+using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
 
 class KVStorePerformanceTest : public Test{
 public:
@@ -48,6 +50,27 @@ public:
       keyValueStore.addValue(key, value);
       expectedValues.emplace(key, value);
     }
+  }
+
+  void TearDown() override{
+    std::cout << "[";
+    for(const auto& v : timeHistogram){
+      std::cout << v << ", ";
+    }
+    std::cout << "]" << std::endl;
+  }
+
+  void updateHistogram(const TimePoint& start, const TimePoint& stop){
+    std::chrono::microseconds elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    if(DEBUG_TEST){
+      // if debugging the test print the double value of debug time to show it's not trivially 0.0
+      std::chrono::duration<double> debugTime{start - stop};
+      std::cout << debugTime.count() << std::endl;
+
+      ASSERT_TRUE(elapsedTime.count() >= 0);
+    }
+    size_t bin = elapsedTime.count()/microsecondResolution;
+    timeHistogram[bin] += 1;
   }
 
   std::unordered_map<size_t, double> expectedValues;
@@ -62,34 +85,65 @@ public:
 TEST_F(KVStorePerformanceTest, AccuracyAndTimeTest){
   auto index{0u};
 
-  auto update_histogram = [this](const auto& start, const auto& stop){
-    std::chrono::microseconds elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    if(DEBUG_TEST){
-      // if debugging the test print the double value of debug time to show it's not trivially 0.0
-      std::chrono::duration<double> debugTime{start - stop};
-      std::cout << debugTime.count() << std::endl;
-
-      ASSERT_TRUE(elapsedTime.count() >= 0);
-    }
-    size_t bin = elapsedTime.count()/microsecondResolution;
-    timeHistogram[bin] += 1;
-  };
-
   for(const auto& [key, value] : expectedValues){
     auto start = std::chrono::system_clock::now();
     auto result = keyValueStore.getValue(key);
     auto stop = std::chrono::system_clock::now();
-    update_histogram(start, stop);
+    updateHistogram(start, stop);
     double actualValue{};
     testAndCast(result, actualValue);
     EXPECT_EQ(value, actualValue) << index;
     ++index;
   }
-  std::cout << "[";
-  for(const auto& v : timeHistogram){
-    std::cout << v << ", ";
+}
+
+// In my environment the KVStorePerformanceTest takes approximately 20 seconds to run, so allowing deadlines up to 60
+// seconds in length
+std::uniform_int_distribution<> deadlineDistribution{0, DEBUG_TEST ? 10 : 60000};
+
+class KVStorePerformanceWithDeadlinesTest : public KVStorePerformanceTest{
+public:
+  void SetUp() override{
+    const auto start = std::chrono::system_clock::now();
+    for(auto i = 0u; i < pairCount; ++i){
+      auto key = keyDistribution(rng);
+      auto value = valueDistribution(rng);
+      auto lifetime = std::chrono::milliseconds{deadlineDistribution(rng)};
+      auto deadline = std::chrono::system_clock::now() + lifetime;
+      keyValueStore.addValue(key, value, lifetime);
+      expectedValues.emplace(key, value);
+      expectedDeadlines.emplace(key, deadline);
+    }
+    const auto stop = std::chrono::system_clock::now();
+    const std::chrono::duration<double> elapsedTime = stop - start;
+    std::cout << "Set Up elapsed time: " << elapsedTime.count() << std::endl;
   }
-  std::cout << "]" << std::endl;
+
+  std::unordered_map<size_t, std::chrono::time_point<std::chrono::system_clock>> expectedDeadlines;
+};
+
+TEST_F(KVStorePerformanceWithDeadlinesTest, deadlineChecked){
+  // we've already checked above that the values are consistent, this test just confirms deadline expectations
+  auto expiredCount{0u};
+  for(const auto& [key, value] : expectedValues){
+    auto start = std::chrono::system_clock::now();
+    auto result = keyValueStore.getValue(key);
+    auto stop = std::chrono::system_clock::now();
+    updateHistogram(start, stop);
+    if(expectedDeadlines.at(key) < start){
+      if(DEBUG_TEST){
+        std::cout << ".";
+      }
+      ++expiredCount;
+      EXPECT_FALSE(result.has_value());
+    } else {
+      if(DEBUG_TEST){
+        std::cout << ",";
+      }
+      EXPECT_TRUE(result.has_value());
+    }
+  }
+  std::cout << "Encountered " << expiredCount << " expired entries.";
 }
 
 } // namespace
