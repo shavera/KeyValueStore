8c33d60b792c5d64da201bc22ccc0b20d5dcb4e2
Alex Shaver <alexshaver@gmail.com>
2021-04-25 11:50:18 -0400

    Sketch basic API for KVStore

--- src/KeyValueStore.cpp
+++ src/KeyValueStore.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "KeyValueStore/KeyValueStore.h"
+
+void KeyValueStore::addValue(
+    int64_t key, std::any value, KeyValueStore::OptionalDuration pairLifetime) {
+
+}
+
+std::optional<std::any> KeyValueStore::getValue(int64_t key) {
+  return std::optional<std::any>();
+}
--- src/include/KeyValueStore/KeyValueStore.h
+++ src/include/KeyValueStore/KeyValueStore.h
@@ -5,8 +5,25 @@
 #ifndef KEYVALUESTORE_KEYVALUESTORE_H
 #define KEYVALUESTORE_KEYVALUESTORE_H
 
-class KeyValueStore {
+#include <any>
+#include <chrono>
+#include <optional>
+#include <unordered_map>
 
+class KeyValueStore {
+public:
+  using OptionalDuration = std::optional<std::chrono::milliseconds>;
+  void addValue(int64_t key, std::any value, OptionalDuration pairLifetime);
+
+  std::optional<std::any> getValue(int64_t key);
+
+private:
+  using OptionalDeadline =
+      std::optional<std::chrono::time_point<std::chrono::system_clock>>;
+
+  using ValuePair = std::pair<std::any, OptionalDeadline>;
+
+  std::unordered_map<int64_t, ValuePair> store_;
 };
 
 #endif // KEYVALUESTORE_KEYVALUESTORE_H
